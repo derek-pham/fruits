ab5f9aa6d1422e35418a5da5c00d5939e735ae12
derek-pham <derekpham3@gmail.com>
2024-09-29 19:41:23 +1000

    - reset values

--- src/scenes/Interface/InterfaceContext.jsx
+++ src/scenes/Interface/InterfaceContext.jsx
@@ -6,44 +6,9 @@ const InterfaceContext = createContext();
 export const useInterfaceContext = () => useContext(InterfaceContext);
 
 export const InterfaceContextProvider = ({ children }) => {
-    const [triggerNoteFor, setTriggerNoteFor] = useState('null')
-    const [cancelHighlights, setCancelHighlights] = useState(false)
-    const [buttonMenu, setButtonMenu] = useState([])
-    const [loadingStatus, setLoadingStatus] = useState(0)
-    const [viewedCards, setViewedCards] = useState({
-        photo: false,
-        globe: false,
-        flask: false,
-        coffee: false
-    })
-
-    const addToButtonMenu = (newItem) => {
-        if (!buttonMenu.includes(newItem)) {
-            setButtonMenu((prevItems) => [...prevItems, newItem]);
-        }
-    }
-
-    const renderButton = (name) => {
-        if (buttonMenu.includes(name)) {
-            return true
-        }
-        else return false
-    }
 
     return (
         <InterfaceContext.Provider value={{
-            triggerNoteFor,
-            setTriggerNoteFor,
-            cancelHighlights,
-            setCancelHighlights,
-            buttonMenu,
-            setButtonMenu,
-            addToButtonMenu,
-            renderButton,
-            loadingStatus,
-            setLoadingStatus,
-            viewedCards,
-            setViewedCards
         }}
         >
             {children}
